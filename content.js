57afe8878a05a4146557761c0d4cfa92a49245f2
Nwakaego Glory Nwaekpe <81041326+Nwakaego-Ego@users.noreply.github.com>
2026-08-25 11:06:41 -0700

    show save button at highlighted text

--- content.js
+++ content.js
@@ -1,16 +1,29 @@
- function detectHighlight () {
-   
-document.addEventListener("mouseup", function(){
-    const saveWords = window.getSelection().toString()
-    if(saveWords.length > 0){
-        console.log(saveWords)
-    } else {return}
-}) 
- }
+function detectHighlight() {
+  document.addEventListener("mouseup", function (event) {
+    const saveWords = window.getSelection().toString();
+    if (saveWords.length === 0) {
+      return;
+    }
 
-  detectHighlight()
+    const savedBtn = document.getElementById("kerna-save-btn");
+    if (savedBtn) {
+      savedBtn.style.left = event.clientX + "px";
+      savedBtn.style.top = event.clientY + "px";
+    } else {
+      const createBtn = document.createElement("div");
+      createBtn.id = "kerna-save-btn";
+      createBtn.style.backgroundColor = "red";
+      createBtn.style.position = "fixed";
+      createBtn.textContent = "save-to-kerna";
+      createBtn.style.padding = "12px";
+      createBtn.style.zIndex = "9999";
+      createBtn.style.width = "20px";
+      createBtn.style.height = "20px";
+      document.body.appendChild(createBtn);
+      createBtn.style.left = event.clientX + "px";
+      createBtn.style.top = event.clientY + "px";
+    }
+  });
+}
 
-
-
-
- 
+detectHighlight();
