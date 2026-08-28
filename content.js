1b47afb83e2329bc9b093ed1579caddb5e240abe
Nwakaego Glory Nwaekpe <81041326+Nwakaego-Ego@users.noreply.github.com>
2026-08-28 10:05:41 -0700

    save highlighted text and source URL

--- content.js
+++ content.js
@@ -1,16 +1,28 @@
 function detectHighlight() {
+  let currentSelection = "";
   document.addEventListener("mouseup", function (event) {
+    // document.getElementById("kerna-save-btn") asks the DOM to find the element carrying that ID. This prevents Kerna from creating duplicate buttons.
+    //event.target.id === "kerna-save-btn" reads the ID of the element where mouseup originated. This distinguishes clicking Kerna from releasing the mouse over normal webpage content.//
+    if (event.target.id === "kerna-save-btn") {
+      return;
+    }
+
     const saveWords = window.getSelection().toString();
     if (saveWords.length === 0) {
       return;
     }
 
+    currentSelection = saveWords;
+
     const savedBtn = document.getElementById("kerna-save-btn");
+
+    // the event.client y and x gives the position of the mouse so that the dom knows exaclty where to put the button on the screen.
+
     if (savedBtn) {
       savedBtn.style.left = event.clientX + "px";
       savedBtn.style.top = event.clientY + "px";
     } else {
-      const createBtn = document.createElement("div");
+      const createBtn = document.createElement("button");
       createBtn.id = "kerna-save-btn";
       createBtn.style.backgroundColor = "red";
       createBtn.style.position = "fixed";
@@ -22,8 +34,23 @@ function detectHighlight() {
       document.body.appendChild(createBtn);
       createBtn.style.left = event.clientX + "px";
       createBtn.style.top = event.clientY + "px";
+
+      createBtn.addEventListener("click", function () {
+        const entry = {
+          text: currentSelection,
+          sourceUrl: window.location.href,
+        };
+        const storageData = {
+          latestEntry: entry,
+        };
+        chrome.storage.local.set(storageData);
+      });
     }
   });
 }
 
 detectHighlight();
+
+// createBtn.id gives the element its identity.
+// event.target.id reveals the identity of the event’s origin.
+// getElementById searches the DOM using that identity.
